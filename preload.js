1f78b9ff52f37bc7ce77df698f9a6c9562dfc2ff
Hamza Awan <70629275+HackHobby-Lab@users.noreply.github.com>
2025-08-11 16:09:57 +0500

    made the action build file more manageable

--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@ const { contextBridge, ipcRenderer, shell } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   // openExternal: (url) => shell.openExternal(url),
-  //   openExternal: (url) => shell.openExternal(link.url),
+    // openExternal: (url) => shell.openExternal(link.url),
   openSettings: () => ipcRenderer.send('open-settings'),
   onUpdateLinks: (cb) => ipcRenderer.on('update-links', (e, links) => cb(links)),
   onLinksUpdated: (callback) => ipcRenderer.on('links-updated', (event, links) => callback(links))
